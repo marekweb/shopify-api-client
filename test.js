10fe2a096f067a7cca86ad56941a898d8889dba8
Mary Scale <scalemary@gmail.com>
2017-06-09 10:46:28 -0400

    added some comments to test file

--- test.js
+++ test.js
@@ -9,15 +9,23 @@ var ShopifyClient = new shopifyClient({
     'accessToken': process.env.SHOPIFY_ACCESS_TOKEN
     });
 
-//ShopifyClient.getShop();
 
+//Makes multiple calls to the shopify API
+//Used to test call limit quantity and frequency
 function multipleShopifyCalls(numberOfCalls) {
     for (var i = 0; i < numberOfCalls; i++) {
+        //can set a timeout function to any interval between calls
+
         //setTimeout(function () {
         //    ShopifyClient.getShop()
         //}, 100 * i)
+
+        //use the getShop function to do a general get request
         ShopifyClient.getShop();
     }
 }
 
-multipleShopifyCalls(41);
+//multipleShopifyCalls(41);
+
+//GET shop
+ShopifyClient.getShop();
