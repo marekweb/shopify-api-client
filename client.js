bd5ce21ec3dc893c1bc1eecfecad573943d39908
Marek <marekz@gmail.com>
2017-06-10 14:59:29 -0400

    Change var to const and code style changes

--- client.js
+++ client.js
@@ -1,8 +1,7 @@
-var Promise = require('bluebird');
 const got = require('got');
-var CheckOptions = require('check-options');
-var Promise = require('bluebird');
-var debug = require('debug')('shopify-client');
+const checkOptions = require('check-options');
+const Promise = require('bluebird');
+const debug = require('debug')('shopify-`client');
 
 // Utility function to get a particular property, used with .then()
 function accessProperty(property) {
@@ -28,7 +27,7 @@ function deletedProductId(id) {
 
 module.exports = class ShopifyClient {
   constructor(options) {
-    CheckOptions(options, ['hostname', 'accessToken']);
+    checkOptions(options, ['hostname', 'accessToken']);
 
     let { hostname, accessToken } = options;
 
@@ -39,14 +38,14 @@ module.exports = class ShopifyClient {
   makeRequest(method, path, data = {}) {
     method = method.toUpperCase();
 
-    var url = `https://${this.hostname}/admin/${path}`;
+    const url = `https://${this.hostname}/admin/${path}`;
 
     debug(`${method} ${url}`);
 
     //amount of milliseconds to delay promise chain
-    var delayMilliSecs = 0;
+    const delayMilliSecs = 0;
 
-    var requestOptions = {
+    const requestOptions = {
       method: method,
       //url: url,
       headers: {
@@ -70,7 +69,7 @@ module.exports = class ShopifyClient {
 
       //console.log(response.statusCode);
       console.log(response.headers['x-shopify-shop-api-call-limit']);
-      var callLimit = response.headers['x-shopify-shop-api-call-limit'];
+      let callLimit = response.headers['x-shopify-shop-api-call-limit'];
 
       if (callLimit) {
         callLimit = parseInt(callLimit.split('/'));
@@ -91,7 +90,7 @@ module.exports = class ShopifyClient {
     //return request(requestOptions).then(response => {
     //    // debug(response.request.uri.href);
 
-    //    var callLimit = response.headers['x-shopify-shop-api-call-limit'];
+    //    const callLimit = response.headers['x-shopify-shop-api-call-limit'];
     //    if (callLimit) {
     //        callLimit = parseInt(callLimit.split('/'));
     //    }
@@ -107,11 +106,10 @@ module.exports = class ShopifyClient {
 
   makeRequestWithRetry(method, path, data = {}) {
     // Create a function that we can call recursively
-    var that = this;
     //console.log(this.makeRequest)
-    function tryRequest() {
+    const tryRequest = () => {
       //console.log(that.makeRequest)
-      return that.makeRequest(method, path, (data = {})).catch(err => {
+      return this.makeRequest(method, path, (data = {})).catch(err => {
         if (err.response.statusCode === 429) {
           return Promise.delay(500).then(tryRequest);
         }
@@ -155,6 +153,7 @@ module.exports = class ShopifyClient {
     return this.makeRequest('post', `products.json`, { product })
       .then(accessProperty('product'))
       .catch(error => {
+        console.log(error);
         console.log(error.response.body);
       });
   }
@@ -173,7 +172,7 @@ module.exports = class ShopifyClient {
       .catch(error => {
         console.log(error);
       });
-  } /*accessProperty({ delete: 'productDelete', id: id})*/
+  }
 
   getProductVariant(id) {
     return this.makeRequest('get', `variants/${id}.json`)
@@ -216,33 +215,33 @@ module.exports = class ShopifyClient {
   }
 
   listRecurringApplicationCharges() {
-    var url = `recurring_application_charges.json`;
+    const url = `recurring_application_charges.json`;
     return this.makeRequest('get', url).then(
       accessProperty('recurring_application_charges')
     );
   }
 
   getRecurringApplicationCharge(chargeId) {
-    var url = `recurring_application_charges/${chargeId}`;
+    const url = `recurring_application_charges/${chargeId}`;
     return this.makeRequest('get', url).then(
       accessProperty('recurring_application_charge')
     );
   }
 
   activateRecurringApplicationCharge(chargeId) {
-    var url = `recurring_application_charges/${chargeId}/activate.json`;
+    const url = `recurring_application_charges/${chargeId}/activate.json`;
     return this.makeRequest('post', url);
   }
 
   cancelRecurringApplicationCharge(chargeId) {
-    var url = `recurring_application_charges/${chargeId}.json`;
+    const url = `recurring_application_charges/${chargeId}.json`;
     return this.makeRequest('delete', url);
   }
 
   createUsageCharge(recurringApplicationChargeId, name, amount) {
-    var url = `recurring_application_charges/${recurringApplicationChargeId}/usage_charges.json`;
+    const url = `recurring_application_charges/${recurringApplicationChargeId}/usage_charges.json`;
 
-    var usageCharge = {
+    const usageCharge = {
       usage_charge: {
         description: name,
         price: amount
@@ -255,19 +254,19 @@ module.exports = class ShopifyClient {
   }
 
   getScriptTags() {
-    var url = 'script_tags.json';
+    const url = 'script_tags.json';
     return this.makeRequest('get', url).then(accessProperty('script_tags'));
   }
 
   deleteScriptTag(id) {
-    var url = `script_tags/${id}.json`;
+    const url = `script_tags/${id}.json`;
     return this.makeRequest('delete', url);
   }
 
   setSingleScriptTag(url) {
-    var needToCreate = true;
+    const needToCreate = true;
     return this.getScriptTags().then(scriptTags => {
-      var requestPromises = scriptTags.map(scriptTag => {
+      const requestPromises = scriptTags.map(scriptTag => {
         if (scriptTag.src === url) {
           needToCreate = false;
           return null;
@@ -285,8 +284,8 @@ module.exports = class ShopifyClient {
   }
 
   createScriptTag(src) {
-    var url = 'script_tags.json';
-    var requestData = {
+    const url = 'script_tags.json';
+    const requestData = {
       script_tag: {
         event: 'onload',
         src: src
@@ -302,7 +301,7 @@ module.exports = class ShopifyClient {
   }
 
   createWebhook(topic, address) {
-    var requestData = {
+    const requestData = {
       webhook: {
         topic: topic,
         address: address,
@@ -313,13 +312,13 @@ module.exports = class ShopifyClient {
   }
 
   deleteWebhook(id) {
-    var url = `webhooks/${id}.json`;
+    const url = `webhooks/${id}.json`;
     return this.makeRequest('delete', url);
   }
 
   deleteAllWebhooks() {
-    var _this = this;
-    var deletionPromises = this.getWebhooks().map(function(webhook) {
+    const _this = this;
+    const deletionPromises = this.getWebhooks().map(function(webhook) {
       return _this.deleteWebhook(webhook.id);
     });
 
@@ -328,7 +327,7 @@ module.exports = class ShopifyClient {
 
   setWebhooks(desiredWebhooks) {
     return this.deleteAllWebhooks().then(() => {
-      var creationPromises = desiredWebhooks.map(webhook => {
+      const creationPromises = desiredWebhooks.map(webhook => {
         return this.createWebhook(webhook.topic, webhook.address);
       });
 
@@ -342,7 +341,7 @@ module.exports = class ShopifyClient {
 
   getOrders(fields) {
     // TODO make this return only what's necessary
-    var requestData = {
+    const requestData = {
       limit: 250,
       // fields: 'financial_status,total_price,currency', // TODO line_items
       // 'created_at_min': '2016-08-01T00:00-4:00',
